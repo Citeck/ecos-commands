1e3c086a07caae067b723bbf6bc2f0a3e8ef7912
Roman.Makarskiy <roman.makarskiy@citeck.ru>
2021-08-26 17:36:38 +0300

    fix SimpleCommandCtxController, set must return correct value

--- ecos-commands/src/main/java/ru/citeck/ecos/commands/context/SimpleCommandCtxController.kt
+++ ecos-commands/src/main/java/ru/citeck/ecos/commands/context/SimpleCommandCtxController.kt
@@ -2,11 +2,11 @@ package ru.citeck.ecos.commands.context
 
 class SimpleCommandCtxController : CommandCtxController {
 
-    override fun setCurrentUser(user: String) : String = ""
+    override fun setCurrentUser(user: String) : String = user
 
     override fun getCurrentUser() : String = ""
 
-    override fun setCurrentTenant(tenant: String) : String = ""
+    override fun setCurrentTenant(tenant: String) : String = tenant
 
     override fun getCurrentTenant() : String = ""
 
