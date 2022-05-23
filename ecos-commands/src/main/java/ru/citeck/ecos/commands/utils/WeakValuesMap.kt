5e4eac023567bc3b95913ca3b60fffbbdf4c1bc4
Pavel Simonov <pavel.simonov@citeck.ru>
2022-05-23 11:57:02 +0300

    ECOSCOM-4684 - NullPointerException in commands lib (cherry picked from commit 6a9d84469e170978e47e061912e7dcd59af007cb)

--- ecos-commands/src/main/java/ru/citeck/ecos/commands/utils/WeakValuesMap.kt
+++ ecos-commands/src/main/java/ru/citeck/ecos/commands/utils/WeakValuesMap.kt
@@ -27,7 +27,9 @@ class WeakValuesMap<K, V> {
         val oldRef = refsQueue.poll()
         if (oldRef != null) {
             val oldKey = keyByRef.remove(oldRef)
-            data.remove(oldKey)
+            if (oldKey != null) {
+                data.remove(oldKey)
+            }
         }
 
         val ref = WeakReference(value, refsQueue)
